cef0542011226511430261dae6aa3b1c2f1f6ded
wliu <wliu0709@gmail.com>
2021-06-21 13:43:25 -0400

    phase1

---
+++ Minesweeper/components/game.jsx
@@ -0,0 +1,29 @@
+import * as Minesweeper from '../minesweeper';
+
+class Game extends React.Component{
+
+    constructor(props){
+        super(props);
+        const board = new Minesweeper.Board(9,9);
+        this.state = { 
+            board:board
+        };
+
+        this.updateGame = this.updateGame.bind(this);
+    }
+
+    updateGame(){
+
+    }
+
+    render(){
+        
+        return (
+                <Board board={this.state.board} updateGame={this.updateGame}/>
+        
+        );
+    }
+
+}
+
+export default Game;
